0f2edf016a1e202a78eab971ac4bb4446599b0f7
zzz1 <z@example.com>
2021-12-18 23:14:18 +0200

    now assembles pack and prints forge link

--- README.md
+++ README.md
@@ -1,23 +1,20 @@
 # Minecraft modpack downloader
 
-This script downloads the mods for a modpack using the manifest file
+This script downloads the files for a modpack using it's zip file
 
 ### Usage
-  python3 downloader.py [manifest_path]
+  python3 downloader.py [pack_path].zip
+  - it's going to download and assemble the pack folder in the current directory
 
-#### Manifest file
+#### Pack zip
   - find the modpack on curseforge.com go to: Files -> click on version.zip -> download
-  - uzip and the manifest is in there
 
 #### Build modpack
-  - create a folder in ~/.minecraft/modded/[name_of_modpack]
-  - put in the mods/ folder that the script downloads
-  - put in the files from overrides(found in the modpack zip), no need for mods/
-  
-  - get correct version of forge(version in manifest) and install it
+  - you can put the pack folder somewhere in the game folder ex: `~/.minecraft/modded/[name_of_modpack]`
+  - install the correct forge version(the script prints the link where to download it at the end)
   
   - in the minecraft launcher
-    - create new installation with correct version of curse 
+    - create new installation with correct version of forge 
       - select the game directory to the modpack path
       - you can also change the java args here to increase ram
   
--- downloader.py
+++ downloader.py
@@ -1,24 +1,26 @@
 #!/usr/bin/env python3
 
-import requests
 import itertools
+import pathlib
 import json
 import sys
 import os
 
 from concurrent.futures import ThreadPoolExecutor
 
+import requests
+
 # This is depricated
 MOD_INFO_URL = 'https://cursemeta.dries007.net'
+FORGE_URL_TEMPLATE = "https://maven.minecraftforge.net/net/minecraftforge/forge/%s-%s/forge-%s-%s-installer.jar"
 
-def download_mod(mod_details, index, mod_count):
+def download_mod(mod_details, index, mod_count, mods_path):
     print('Downloading...(%d/%d)' % (index, mod_count))
 
     mod_data = get_mod_data(mod_details['projectID'], mod_details['fileID'])
-    print(mod_data)
     download_url = mod_data['DownloadURL']
     file_name = mod_data['FileNameOnDisk']
-    download_file(download_url, 'mods/' + file_name)
+    download_file(download_url, mods_path / file_name)
 
 def get_mod_data(project_id, file_id):
     url = '%s/%d/%d.json' % (MOD_INFO_URL, project_id, file_id)
@@ -36,14 +38,40 @@ def load_manifest(manifest_path):
     with open(manifest_path) as man_f:
         return json.load(man_f)
 
-if __name__ == '__main__':
-    manifest = load_manifest(sys.argv[1])
-    os.mkdir('mods')
+def download_mods(manifest, to_path):
+    to_path.mkdir(exist_ok=True)
 
     mod_count = len(manifest['files'])
 
     print("Starting mods download")
     with ThreadPoolExecutor(max_workers=5) as executor:
-        executor.map(download_mod, manifest['files'], itertools.count(1, 1), itertools.repeat(mod_count))
-    print("Done")
+        executor.map(download_mod, manifest['files'], itertools.count(1, 1),\
+                itertools.repeat(mod_count), itertools.repeat(to_path))
+    print("Done downloading mods")
+
+def print_forge_download_url(manifest):
+    game_version = manifest["minecraft"]["version"]
+    forge_version = manifest["minecraft"]["modLoaders"][0]["id"]
+
+    print("\nNow get and install forge: %s\n" %\
+            (FORGE_URL_TEMPLATE % (game_version, forge_version[6:], game_version, forge_version[6:])))
+
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print("Usage: python3 downloader.py [zip_path]")
+        sys.exit(0)
+
+    zip_path = pathlib.Path(sys.argv[1])
+
+    pack_dir = pathlib.Path(__file__).parent / zip_path.stem
+    pack_dir.mkdir()
+
+    # Unzip in pack dir and copy overrides over
+    os.system("unzip %s -d %s/" % (zip_path, pack_dir))
+    os.system("mv %s/* %s" % (pack_dir / "overrides/", pack_dir))
+    os.system("rm -rf %s" % (pack_dir / "overrides/"))
+
+    manifest = load_manifest(pack_dir / "manifest.json")
+    download_mods(manifest, pack_dir / "mods/")
+    print_forge_download_url(manifest)
 
